377f154d8b3b8b681465568ac93694ca00704d02
anurag97cdac <anurag018@outlook.com>
2026-04-20 12:27:38 +0530

    Extent Manager Updated

--- src/test/java/com/sdet/utils/ExtentManager.java
+++ src/test/java/com/sdet/utils/ExtentManager.java
@@ -12,8 +12,10 @@ public class ExtentManager {
     public static ExtentReports getInstance() {
         if(extent ==  null){
             ExtentSparkReporter sparkReporter = new ExtentSparkReporter("test-output/ExtentReport.html");
+            sparkReporter.config().setCss("r-img { width: 50%; }");
             sparkReporter.config().setReportName("Automation Report");
             sparkReporter.config().setDocumentTitle("Test Results");  
+            sparkReporter.config().setEncoding("utf-8");
             extent = new ExtentReports();
             extent.attachReporter(sparkReporter);
         }
